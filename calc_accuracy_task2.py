4290148ee29a111ea0c2d03c45266ec626540b3e
abhishek.agrawal <abhishek.agrawal@eagleview.com>
2023-09-03 14:01:48 +0530

    result path as argument

--- calc_accuracy_task2.py
+++ calc_accuracy_task2.py
@@ -1,8 +1,13 @@
+"""
+Run command - 
+python calc_accuracy_task2.py --result_path ../task2/output/results.txt
+"""
 from Levenshtein import ratio as lev_ratio
 import multiprocessing as mp
 
 
 def main(gt, pred, result):
+
     ratio = lev_ratio(gt, pred)
     if ratio == 1:
         result["100%"] = result.get("100%", 0) + 1
@@ -22,11 +27,15 @@ def main(gt, pred, result):
 
 
 if __name__ == '__main__':
-    result_path = "output/results.txt"
+    import argparse
+    parser = argparse.ArgumentParser("Digit recognition accuracy calculation")
+    parser.add_argument("--result_path", type=str,
+                        help = "path to txt in which filename and prediction is written")
+    args = parser.parse_args()
     gts   = []
     preds = []
     result = {}
-    for line in open(result_path).readlines():
+    for line in open(args.result_path).readlines():
         print(line)
         line =  line.strip("\n")
         # gt   = line.split("\t")[0].split("_")[0].lower()
